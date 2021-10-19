723eb88f97b340b5801867888446004648807c48
Jordan <jordan.rolph@gmail.com>
2021-10-19 18:57:31 +0100

    Add a simple 3x3 board with layout and hover effects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 
+const Square = ({ index, value }) => {
+  return <button className={styles.square}>{value}</button>;
+};
+
+const Board = ({ squares }) => {
+  return (
+    <div className={styles.board}>
+      {squares.map((square) => (
+        <Square key={square.index} index={square.index} value={square.value} />
+      ))}
+    </div>
+  );
+};
+
+const generateEmptyBoard = () => {
+  return [...Array(3 * 3)].map((k, i) => ({ index: i, value: null }));
+};
+
 function App() {
-  return <div className={styles.app}>Hello world</div>;
+  const [squares, setSquares] = useState(generateEmptyBoard());
+
+  return (
+    <div className={styles.app}>
+      <Board squares={squares} />
+    </div>
+  );
 }
 
 export default App;
